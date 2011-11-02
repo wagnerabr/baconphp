ac0131a436f70944f3bf8e1e4bd558c20e9fa631
Zizaco Zizuini <zizaco@gmail.com>
2011-11-01 21:24:44 -0300

    Removed some irrelevant code

--- core/notifications.php
+++ core/notifications.php
@@ -1,76 +1,7 @@
 <?php
-	$errors = array();
-	$warnings = array();
-	$messages = array();
-
-	function HandleError($where, $msg)
-	{
-		global $errors;
-
-		$i = count($errors);
-
-		$errors[$i] = $where.": ".$msg;
-	}
-
-	function ShowErrors()
-	{
-		global $errors;
-		$ret = "";
-
-		foreach($errors as $msg)
-		{
-			$ret = $ret.$msg."\n";;
-		}
-
-		return $ret;
-	}
-
-	function HandleWarning($where, $msg)
-	{
-		global $warnings;
-
-		$i = count($warnings);
-
-		$warnings[$i] = $where.": ".$msg;
-	}
-
-	function ShowWarnings()
-	{
-		global $warnings;
-		$ret = "";
-
-		foreach($warnings as $msg)
-		{
-			$ret = $ret.$msg."\n";;
-		}
-
-		return $ret;
-	}
-
-	function HandleMessage($where, $msg)
-	{
-		global $messages;
-
-		$i = count($messages);
-
-		$messages[$i] = $where.": ".$msg;
-	}
-
-	function ShowMessages()
-	{
-		global $messages;
-		$ret = "";
-
-		foreach($messages as $msg)
-		{
-			$ret = $ret.$msg."\n";;
-		}
-
-		return $ret;
-	}
 	function DebugMsg($msg, $var_dump = false)
 	{
-		echo "<div class='notification debug'>";
+		echo "<div class='alert-message warning'>";
 		if($var_dump)
 		{
 			echo "<pre>";
