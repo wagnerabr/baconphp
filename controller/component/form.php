4b320660b8aacdb2e2565904d4cb1f3f767260a0
Zizaco Zizuini <zizaco@gmail.com>
2011-11-03 13:53:53 -0300

    Improved image handling

--- controller/component/form.php
+++ controller/component/form.php
@@ -45,44 +45,64 @@
 			return $record;
 		}
 
-		public function validate_form_image($model_name, $record)
+		public function validate_form_image($model_name, &$record)
 		{
-			if(isset($_FILES))
+			$theModel = model($model_name);
+			if(is_array($theModel->image))
 			{
-				unset($record['image']);
+				$record["image"]=model($model_name)->image[0]."/default.jpg";
+			}else{
+				return $record;
+			}
 
-				$ext = substr($_FILES['image']['name'],-3);
-				$destiny = IMAGE_PATH.model($model_name)->image[0]."/pending.jpg";
-				$source_image = $_FILES['image']['tmp_name'];
-				if($ext == "jpg" || $ext == "peg")
-				{
-					$source_image = imagecreatefromjpeg($source_image);
-				}elseif($ext == "png"){
-					$source_image = imagecreatefrompng($source_image);
-				}elseif($ext == "gif"){
-					$source_image = imagecreatefromgif($source_image);
-				}elseif($ext == "bmp"){
-					$source_image = imagecreatefromwbmp($source_image);
-				}else{
+			if(isset($_FILES) && $_FILES['image']['name'] != "")
+			{
+				try{
+
+					$ext = substr($_FILES['image']['name'],-3);
+					$destiny = IMAGE_PATH.$theModel->image[0]."/pending.jpg";
+					$source_image = $_FILES['image']['tmp_name'];
+					if($ext == "jpg" || $ext == "peg")
+					{
+						$source_image = imagecreatefromjpeg($source_image);
+					}elseif($ext == "png"){
+						$source_image = imagecreatefrompng($source_image);
+					}elseif($ext == "gif"){
+						$source_image = imagecreatefromgif($source_image);
+					}elseif($ext == "bmp"){
+						$source_image = imagecreatefromwbmp($source_image);
+					}else{
+						return false;
+					}
+
+					if($ext != "jpg" && $ext != "png" && $ext != "bmp" && $ext != "gif")
+					{
+						return false;
+					}
+
+					list($width, $height) = getimagesize($_FILES['image']['tmp_name']);
+					$smalldimen = $width < $height ? $width : $height;
+
+					$xx = ($width / 2) - $smalldimen / 2;
+					$yy = ($height / 2) - $smalldimen / 2;
+
+					$resized = imagecreatetruecolor($theModel->image[1], $theModel->image[2]);
+					if(!imagecopyresampled($resized, $source_image, 0, 0, $xx, $yy, $theModel->image[1], $theModel->image[2], $smalldimen, $smalldimen))
+					{
+						return false;
+					}
+
+					if(!imagejpeg($resized, $destiny, 100))
+					{
+						return false;
+					}
+				}catch(BaconException $ex){
+					$ex->showError("Error while uploading image.");
 					return false;
 				}
-
-				if($ext != "jpg" && $ext != "png" && $ext != "bmp" && $ext != "gif")
-					return false;
-
-				list($width, $height) = getimagesize($_FILES['image']['tmp_name']);
-
-				//if(!move_uploaded_file($_FILES['image']['tmp_name'],$destiny."\\".$file_name)){
-				$resized = imagecreatetruecolor(model($model_name)->image[1], model($model_name)->image[2]);
-				if(!imagecopyresampled($resized, $source_image, 0, 0, 0, 0, model($model_name)->image[1], model($model_name)->image[2], $width, $height))
-					return false;
-
-				if(!imagejpeg($resized, $destiny, 100))
-					return false;
 			}else{
-				return false;
+				return $record;
 			}
-			return $record;
 		}
 	}
 
